026bfa55424a642b3de127ce27212dc5f506edb7
D3VIDE <errandojose03@gmail.com>
2025-11-28 21:01:41 +0700

    Add user sign in/login

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,15 +2,17 @@
 
 import { useState } from "react";
 import { AuthForm } from "../../components/AuthForm";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
+  const router = useRouter();
   const [theme, setTheme] = useState<"light" | "dark">("light");
 
   return (
     <AuthForm
-      onLoginSuccess={() => console.log("Login sukses!")}
       theme={theme}
       onToggleTheme={() => setTheme(theme === "light" ? "dark" : "light")}
+      onLoginSuccess={() => router.push("/")}
     />
   );
 }
